5f9c5a06d360fb9e3e94bf0fc099f6e592c423dc
Kot <karol.sykala@protonmail.com>
2023-01-20 20:17:07 +0100

    add some quotes

---
+++ main.js
@@ -0,0 +1,21 @@
+// make 3 lists with sentences
+
+let goggins_quotes = ["The pain that you are willing to endure is measured by how bad you want it.",
+"We’re either getting better or we’re getting worse.",
+"Denial is the ultimate comfort zone.",
+"It’s time to go to war with yourself.",
+"I don’t stop when I’m tired. I stop when I’m done.",
+"We all have the ability to come from nothing to something.",
+"Life is one big tug of war between mediocrity and trying to find your best self.",
+"The path to success will leave you callused, bruised, and very tired. It will also leave you empowered.",
+"Everybody comes to a point in their life when they want to quit. But it’s what you do at that moment that determines who you are.",
+"We don’t rise to the level of our expectations, we fall to the level of our training.",
+
+];
+let stoics_quotes = [];
+let gratitude_quotes = [];
+
+
+// create a loop that takes one of the sentences from each list and displays it on a screen
+
+
